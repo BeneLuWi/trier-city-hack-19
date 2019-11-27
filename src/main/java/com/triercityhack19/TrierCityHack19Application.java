4cf89bdf6ee6da5e0ba036aa79111d40a7e5086f
Benedikt Lüken-Winkels <s4beluek@uni-trier.de>
2019-11-27 16:01:17 +0100

    init frontend

--- src/main/java/com/triercityhack19/TrierCityHack19Application.java
+++ src/main/java/com/triercityhack19/TrierCityHack19Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrierCityHack10Application {
+public class TrierCityHack19Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(TrierCityHack10Application.class, args);
+        SpringApplication.run(TrierCityHack19Application.class, args);
     }
 
 }
