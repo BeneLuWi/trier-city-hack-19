25175f5f51d91ef0e945e16130f80cf3b34e578d
Benedikt Lüken-Winkels <s4beluek@uni-trier.de>
2019-11-27 10:15:45 +0100

    init

---
+++ src/main/java/com/triercityhack19/TrierCityHack10Application.java
@@ -0,0 +1,13 @@
+package com.triercityhack19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrierCityHack10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrierCityHack10Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/triercityhack19/TrierCityHack10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triercityhack19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrierCityHack10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
