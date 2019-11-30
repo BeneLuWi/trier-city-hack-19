5b275e01ac607cce0848bd4f9cd3f0a202846951
Benedikt Lüken-Winkels <s4beluek@uni-trier.de>
2019-11-30 13:34:08 +0100

    Add myrides

--- src/main/react/tch19-front/src/App.js
+++ src/main/react/tch19-front/src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import User from "./components/user/User";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Sharer from "./components/sharer/Sharer";
+import MyRides from "./components/myRides/MyRides";
 
 const App = () => {
 
@@ -11,10 +12,12 @@ const App = () => {
             <div>
                 <div style={{height:"90vh"}}>
                     <Route path={"/sharer"} render={() => <Sharer/>}/>
+                    <Route path={"/mytours"} render={() => <MyRides/>}/>
                     <Route exact path={"/"} render={() => <User/>}/>
                 </div>
                 <div className={"w3-bar w3-blue"}>
                     <Link to={"/"} className={"w3-button w3-bar-item w3-left"}>Mitfahren</Link>
+                    <Link to={"/mytours"} className={"w3-button w3-bar-item w3-center"}>Meine Fahrten</Link>
                     <Link to={"/sharer"} className={"w3-button w3-bar-item w3-right"}>Fahrt anbieten</Link>
                 </div>
             </div>
---
+++ src/main/react/tch19-front/src/components/myRides/MyRides.js
@@ -0,0 +1,65 @@
+import React, {useState, useEffect} from 'react'
+import axios from "axios";
+import moment from "moment";
+
+const MyRides = ({}) => {
+
+    /*************
+     *
+     *  HOOKS
+     *
+     *************/
+
+    const [tours, setTours] = useState([]);
+
+    useEffect(() => {
+
+    });
+
+    /*************
+     *
+     *  FUNCTIONS
+     *
+     *************/
+
+    const getTours = () => {
+        axios.get("/api/rides")
+            .then(res => setTours(res.data))
+            .catch(err => console.log(err))
+    }
+
+    /*************
+     *
+     *  RENDERING
+     *
+     *************/
+
+
+    return (
+        <div style={{height: "100%"}}>
+            <div className={"w3-blue animate-left"} style={{height: "40%"}}>
+                <h2>Gebuchte Fahrten</h2>
+                <ul className={"w3-ul"}>
+                    {tours.filter(t => t.isGuest).map(tour =>
+                        <li key={tour.id}>
+                            Von {tour.start} nach {tour.dest} um {moment.unix(tour.starttime).format("DD YY HH:mm")}
+                        </li>
+                    )}
+                </ul>
+            </div>
+            <div className={"w3-green animate-right"} style={{height: "40%"}}>
+                <h2>Angebotene Fahrten</h2>
+                <ul className={"w3-ul"}>
+                    {tours.filter(t => t.isDriver).map(tour =>
+                        <li key={tour.id}>
+                            Von {tour.start} nach {tour.dest} um {moment.unix(tour.starttime).format("DD YY HH:mm")}
+                        </li>
+                    )}
+                </ul>
+            </div>
+        </div>
+    )
+
+};
+
+export default MyRides
